0164381f6b1f195e4e86005a8bc81777322fde26
Andrew <andrew.torgesen@gmail.com>
2017-02-22 17:10:44 -0700

    skeleton created

---
+++ BARK.X/newmainXC16.c
@@ -0,0 +1,15 @@
+/*
+ * File:   newmainXC16.c
+ * Authors: Andrew Torgesen
+ *          Robert Monson
+ *          Ben Himes
+ *          Kade Roth
+ *
+ * Created on February 22, 2017, 4:57 PM
+ */
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
